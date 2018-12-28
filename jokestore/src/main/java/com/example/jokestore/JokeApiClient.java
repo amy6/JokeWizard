ab620d7a0cd5cd186b52f7dbec4e5ad73d9e745c
Mahima <mahima.6.ms@gmail.com>
2018-12-28 23:09:20 +0530

    add rest api call for getting jokes

---
+++ jokestore/src/main/java/com/example/jokestore/JokeApiClient.java
@@ -0,0 +1,49 @@
+package com.example.jokestore;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+public class JokeApiClient {
+
+    private static final String BASE_URL = "https://geek-jokes.sameerkumar.website/api";
+    private static Retrofit retrofit;
+
+    //OkHttpClient to enable logging
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+    private static HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    /**
+     * called to get a reference to Retrofit instance
+     *
+     * @return retrofit client to be used for API calls
+     */
+    public static Retrofit getClient() {
+
+        if (retrofit == null) {
+
+            //add logging interceptor
+            httpClient.addInterceptor(loggingInterceptor);
+
+            retrofit = new Retrofit.Builder()
+                    //specify json converter
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
+                    //specify base URL for the API
+                    .baseUrl(BASE_URL)
+                    .build();
+        }
+
+        return retrofit;
+    }
+}
+
+interface JokeApiService {
+
+    @GET
+    Call<String> getJoke();
+}
--- jokestore/src/main/java/com/example/jokestore/JokeStore.java
+++ jokestore/src/main/java/com/example/jokestore/JokeStore.java
@@ -1,8 +1,30 @@
 package com.example.jokestore;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class JokeStore {
 
+    private String joke = "";
+
     public String getJoke() {
-        return "This is a hilarious joke";
+
+        JokeApiService apiService = JokeApiClient.getClient().create(JokeApiService.class);
+        Call<String> jokeCall = apiService.getJoke();
+        jokeCall.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.body() != null) {
+                    joke = response.body();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+
+            }
+        });
+        return joke;
     }
 }

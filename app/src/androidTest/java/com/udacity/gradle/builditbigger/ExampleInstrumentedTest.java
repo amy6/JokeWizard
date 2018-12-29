aa400373adbac03aaaf5672c59dcd92beb7af112
Mahima <mahima.6.ms@gmail.com>
2018-12-29 09:59:54 +0530

    update asynctask test to use countdown latch

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ExampleInstrumentedTest.java
@@ -1,5 +1,7 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -7,17 +9,32 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CountDownLatch;
+
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
-    @Rule
-    public ActivityTestRule activityTestRule = new ActivityTestRule<>(MainActivity.class);
-
     @Test
-    public void checkAsyncTask_nonEmptyStringResult() {
-        String result = new EndpointsAsyncTask().doInBackground(activityTestRule.getActivity());
-        assertNotNull(result);
+    public void checkAsyncTask_nonEmptyStringResult() throws InterruptedException {
+        assertTrue(true);
+        final CountDownLatch latch = new CountDownLatch(1);
+        Context context = InstrumentationRegistry.getContext();
+        EndpointsAsyncTask testTask = new EndpointsAsyncTask(new ProgressChangeListener() {
+            @Override
+            public void onProgressChanged(boolean complete) {
+
+            }
+        }) {
+            @Override
+            protected void onPostExecute(String result) {
+                assertNotNull(result);
+                latch.countDown();
+            }
+        };
+        testTask.execute(context);
+        latch.await();
     }
 }

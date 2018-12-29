e64facf12bb56119bb49c328897b83da796f5059
Mahima <mahima.6.ms@gmail.com>
2018-12-29 16:49:14 +0530

    implement share joke option

--- jokeactivity/src/main/java/com/example/jokeactivity/JokeActivity.java
+++ jokeactivity/src/main/java/com/example/jokeactivity/JokeActivity.java
@@ -1,11 +1,16 @@
 package com.example.jokeactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
+
+    private String joke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +21,32 @@ public class JokeActivity extends AppCompatActivity {
 
         if (getIntent() != null) {
             if (getIntent().hasExtra("joke")) {
-                String joke = getIntent().getStringExtra("joke");
+                joke = getIntent().getStringExtra("joke");
                 if (!TextUtils.isEmpty(joke)) {
                     jokeTextView.setText(joke);
                 }
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_share) {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, joke);
+            intent = Intent.createChooser(intent, "Share joke via");
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

9472cd710381d3bd090ea901b7b64167d64c5c63
Mahima <mahima.6.ms@gmail.com>
2018-12-29 15:31:35 +0530

    update color and font for the app

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,6 +1,5 @@
 package com.udacity.gradle.builditbigger;
 
-import android.app.MediaRouteButton;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,8 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
-
-import com.udacity.gradle.builditbigger.R;
 
 
 /**
@@ -34,6 +31,7 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
+        onProgressChanged(true);
         new EndpointsAsyncTask(this).execute(getContext());
     }
 
